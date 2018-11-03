444a2c8c6cbcb560940b0412810ce5031c20da14
jiangfw <jiangfengwei_2@126.com>
2018-11-03 22:42:26 +0800

    init grunt-jfw plugin

---
+++ tasks/jfw.js
@@ -0,0 +1,53 @@
+/*
+ * jfw
+ * https://github.com/jiangfw/grunt
+ *
+ * Copyright (c) 2018 jiangfw
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('jfw', 'jfw first grunt plugins', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+	console.log("jfw plugin log"+options);
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+	  //此处添加插件的业务功能.
+	  console.log("jfw plugin 业务功能执行完成");
+	  
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
